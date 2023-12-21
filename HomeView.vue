a840ba9c17cea96e93a26230a1217695b0f60ffb
irsyadhab <119821636+irsyadhab@users.noreply.github.com>
2023-12-21 14:43:27 +0700

    Add files via upload

---
+++ HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue'
+const email = ref('')
+const password = ref('')
+
+async function login() {
+    const res = await fetch('http://localhost:3000/api/users/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value,
+        })
+    })
+    const json = await res.json()
+}
+</script>
+
+<template>
+Email: {{ email }} <br>
+Password: {{ password }}
+<h1>Login</h1>
+<input v-model="email" placeholder="email"/><br>
+<input v-model="password" placeholder="password"/><br>
+<button>Login</button>
+</template>
